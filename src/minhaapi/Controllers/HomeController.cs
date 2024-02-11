96cebba4cfb5b839eca6d46ebb212e6859885afe
Wladison Calebe <w_calebe@hotmail.com>
2024-02-11 15:03:53 -0300

    add novo controller

---
+++ src/minhaapi/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace minhaapi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HomeController : ControllerBase
+{
+    [HttpGet(Name = "obterDados")]
+    public IEnumerable<String> obterDados(string valor, int idade)
+    {
+        //fazer qualauer coisa aqui 
+        if (valor.Equals("mica") && idade==10) 
+            yield return "campeao";
+        else yield return "perdedor";
+    }
+}
+
